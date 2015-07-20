f4721814261eb8724e75b4fbb0c4b20cc396bf3a
tk <aoziczero@gmail.com>
2015-07-21 01:09:20 +0900

    -

--- transport/tcp/filters/timeout_filter.hpp
+++ transport/tcp/filters/timeout_filter.hpp
@@ -5,7 +5,7 @@
 #include <buffer/byte_buffer.hpp>
 #include <io/ip/address.hpp>
 #include <diagnostics/tcode_error_code.hpp>
-#include <transport/tcp./filter.hpp>
+#include <transport/tcp/filter.hpp>
 
 namespace tcode { namespace transport { namespace tcp {
 
