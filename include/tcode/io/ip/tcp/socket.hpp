72e4273a5ccfd5c13348b0851d603724651db05c
tk <tk@tcode>
2015-08-26 17:57:18 +0900

    -

--- include/tcode/io/ip/tcp/socket.hpp
+++ include/tcode/io/ip/tcp/socket.hpp
@@ -1,6 +1,10 @@
 #ifndef __tcode_io_ip_tcp_socket_h__
 #define __tcode_io_ip_tcp_socket_h__
 
+#include <tcode/io/ip/ip.hpp>
+
+#include <tcode/function.hpp>
+
 namespace tcode { namespace io { 
 
     class engine;
@@ -11,8 +15,18 @@ namespace ip { namespace tcp {
     public:
         socket( engine& e );
         ~socket( void );
+
+        bool open( int family );
+        void close( const tcode::function< void () >& onclose );
+
+
+        void do_close( ip::socket_type fd );
+
+        ip::socket_type native_handle( void );
+        ip::socket_type native_handle( ip::socket_type handle );
     private:
         engine& _engine;
+        std::atomic< ip::socket_type > _handle;
     };
 
 }}}}
--- src/io/ip/tcp/socket.cpp
+++ src/io/ip/tcp/socket.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <tcode/io/ip/tcp/socket.hpp>
+#include <tcode/io/engine.hpp>
 
 namespace tcode { namespace io { namespace ip { namespace tcp {
 
@@ -10,5 +11,35 @@ namespace tcode { namespace io { namespace ip { namespace tcp {
     socket::~socket( void ) {
 
     }
+
+    bool socket::open( int family ){
+        native_handle( ::socket( family , SOCK_STREAM , IPPROTO_TCP ));
+        return native_handle() != tcode::io::ip::invalid_socket;
+    }
+        
+    void socket::close( const tcode::function< void () >& onclose ){
+        ip::socket_type old = native_handle( tcode::io::ip::invalid_socket );
+        if ( old != tcode::io::ip::invalid_socket )
+            _engine.post( 
+                [this,old,onclose] {
+                    do_close(old); 
+                    onclose(); 
+                });
+    }
+
+    void socket::do_close( ip::socket_type fd ) {
+        _engine.unbind( fd );
+        ::close( fd );
+        // todo drain reqs;
+    }
+
+    ip::socket_type socket::native_handle( void ) {
+        return _handle.load();
+    }
+
+    ip::socket_type socket::native_handle( ip::socket_type handle ){
+        return _handle.exchange( handle );
+    }
+
 
 }}}}
