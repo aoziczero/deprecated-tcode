0c03df538062282b644e4e5f7609ff0ca8e26a1c
tk <aoziczero@gmail.com>
2015-09-24 11:13:06 +0900

    -

--- src/io/rts.cpp
+++ src/io/rts.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include <tcode/error.hpp>
 #include <tcode/io/io.hpp>
-#include <tcode/io/select.hpp>
+#include <tcode/io/rts.hpp>
 #include <tcode/io/engine.hpp>
 #include <tcode/io/operation.hpp>
 #include <tcode/io/ip/option.hpp>
@@ -12,35 +12,39 @@
 #include <tcode/io/ip/tcp/operation_accept.hpp>
 #include <tcode/io/ip/udp/operation_write.hpp>
 #include <tcode/io/ip/udp/operation_read.hpp>
-#include <sys/select.h>
+#include <sys/rts.h>
 #include <unistd.h>
 
 namespace tcode { namespace io {
 
-    struct select::_descriptor{
+    namespace {
+        const int rts_max_event = 256;
+    }
+
+    struct rts::_descriptor{
         int fd;
         tcode::slist::queue< tcode::operation > op_queue[tcode::io::ev_max];
         std::atomic<int> refcount;
 
-        void complete( select* ep ,  int ev );
+        void complete( rts* ep ,  int ev );
 
-        _descriptor( select* ep , int fd  );
+        _descriptor( rts* ep , int fd  );
 
         void add_ref( void );
-        void release( select* ep );
+        void release( rts* ep );
     };
 
-    select::_descriptor::_descriptor( select* ep , int fd ) {
+    rts::_descriptor::_descriptor( rts* ep , int fd ) {
         refcount.store(1);
         ep->_engine.active_inc();
         this->fd = fd;
     }
 
-    void select::_descriptor::add_ref( void ) {
+    void rts::_descriptor::add_ref( void ) {
         refcount.fetch_add(1);
     }
 
-    void select::_descriptor::release( select* ep ){
+    void rts::_descriptor::release( rts* ep ){
         if ( refcount.fetch_sub(1) != 1 ) {
             return; 
         }
@@ -70,74 +74,67 @@ namespace tcode { namespace io {
         delete this;
     }
 
-    void select::_descriptor::complete( select* ep , int events ) {
-        while ( !op_queue[events].empty() ) {
-            io::operation* op = op_queue[events].front<io::operation>();
-            if ( op->post_proc(ep,this) ) {
-                op_queue[events].pop_front();
-                (*op)();
-            } else {
-                break;
-            }
-        }
-    }
-
-    select::select( engine& en )
-        :  _engine( en )
-    {
-    }
-
-    select::~select( void ){
-    }
-
-    int select::run( const tcode::timespan& ts ){
-        fd_set rdfds , wrfds;
-        FD_ZERO( &rdfds );
-        FD_ZERO( &wrfds );
-        fd_set sets[] = { rdfds , wrfds };
-        int ops[] = { ev_read , ev_write };
-        FD_SET( _wake_up.rd_pipe() , &rdfds );
-        int maxfd = _wake_up.rd_pipe();
-        auto it = _desc.begin();
-        while ( it != _desc.end() ) {
-            descriptor desc = *it;
-            for ( int i = 0 ; i < 2 ; ++i ) {
-                if ( !desc->op_queue[ops[i]].empty() ) {
-                    FD_SET( desc->fd , &sets[i] );
-                    if ( maxfd < desc->fd ) 
-                        maxfd = desc->fd;
-                }
-            }
-            ++it;
-        }
-
-        struct timeval tv;
-        tv.tv_sec = static_cast< long >( ts.total_microseconds() / ( 1000 * 1000 ));
-        tv.tv_usec = ts.total_microseconds() % ( 1000 * 1000 );
-        int ret = ::select( maxfd + 1 , &rdfds , &wrfds , nullptr  , &tv );
-        if ( ret <= 0  ) {
-            return 0; 
-        }
-        int run = 0;
-        bool execute_op = FD_ISSET( _wake_up.rd_pipe() , &rdfds );
-        it = _desc.begin();
-        while ( it != _desc.end() ) {
-            descriptor desc = *it;
-            for ( int i = 0 ; i < 2 ; ++i ) {
-                if ( !desc->op_queue[ops[i]].empty() ) {
-                    if ( FD_ISSET( desc->fd , &sets[i] )){
-                        desc->complete( this , ops[i] );
-                        ++run;
+    void rts::_descriptor::complete( rts* ep , int events ) {
+        static const int rtsev[] = {
+            EPOLLIN , EPOLLOUT , EPOLLPRI 
+        };
+        for ( int i = 0 ; i < tcode::io::ev_max ; ++i ) {
+            if ( events & rtsev[i] ){
+                while ( !op_queue[i].empty() ) {
+                    io::operation* op = op_queue[i].front<io::operation>();
+                    if ( op->post_proc(ep,this) ) {
+                        op_queue[i].pop_front();
+                        (*op)();
+                    } else {
+                        break;
                     }
                 }
             }
-            ++it;
+        }
+    }
+
+    rts::rts( engine& en )
+        : _handle( rts_create(rts_max_event))
+        , _engine( en )
+    {
+        struct rts_event e;
+        e.events = EPOLLIN ;
+        e.data.ptr = nullptr;
+        rts_ctl( _handle , EPOLL_CTL_ADD , _wake_up.rd_pipe() , &e );
+    }
+
+    rts::~rts( void ){
+        ::close( _handle );
+    }
+
+    int rts::run( const tcode::timespan& ts ){
+        struct rts_event events[rts_max_event];
+        int nofd  = rts_wait( _handle 
+            , events
+            , rts_max_event
+            , static_cast<int>(ts.total_milliseconds()));
+        if ( nofd <= 0  ) {
+            return 0; 
+        }
+        int run = nofd;
+        bool execute_op = false;
+        for ( int i = 0 ; i < nofd; ++i ){
+            rts::descriptor desc =
+                static_cast< rts::descriptor >( events[i].data.ptr );
+            if ( desc ) {
+                desc->complete( this , events[i].events );
+            } else {
+                char pipe_r[256];
+                ::read( _wake_up.rd_pipe() , pipe_r , 256 );
+                execute_op = true;
+                --run;
+            }
         }
         if ( execute_op ){
             tcode::slist::queue< tcode::operation > ops;
             do {
                 tcode::threading::spinlock::guard guard(_lock);
-                ops = std::move( _op_queue );
+                ops = std::move(_op_queue );
             }while(0);
             while( !ops.empty() ){
                 tcode::operation* op = ops.front();
@@ -149,39 +146,30 @@ namespace tcode { namespace io {
         return run;
     }
     
-    void select::wake_up( void ){
-        char ch=0;
+    void rts::wake_up( void ){
+        char ch = 0;
         ::write( _wake_up.wr_pipe() , &ch , 1 );
     }
 
-    bool select::bind( const descriptor& d ) {
-        execute( tcode::operation::wrap([this,d]{
-                        _desc.push_back(d);
-                    }));
-        return true;
+    bool rts::bind( const descriptor& d ) {
+        struct rts_event e;
+        e.events = EPOLLIN | EPOLLOUT | EPOLLET;
+        e.data.ptr = d;
+        return rts_ctl( _handle , EPOLL_CTL_ADD , d->fd , &e ) == 0;
     }
 
-    void select::unbind( descriptor& d ) {
+    void rts::unbind( descriptor& d ) {
         do { 
             tcode::threading::spinlock::guard guard(_lock);
-            if ( d == nullptr ) 
+            if ( d == nullptr ){
                 return;
-            descriptor desc = nullptr;
-            std::swap( desc , d );
+            }
+            descriptor desc = d;
+            d = nullptr;
             op_add( tcode::operation::wrap( 
                     [this,desc] {
-                        std::size_t i = 0;
-                        for ( ; i < _desc.size() ; ++i ){
-                            if ( _desc[i] == desc )
-                                break;
-                        }
-                        if ( i != _desc.size()) {
-                            if ( i < _desc.size() -1 ) {
-                                std::swap( _desc[i] , _desc[_desc.size() -1 ] );
-                            }
-                            _desc.pop_back();
-                        }
                         if ( desc->fd != -1 ) {
+                            rts_ctl( _handle , EPOLL_CTL_DEL , desc->fd , nullptr );
                             ::close( desc->fd );
                             desc->fd = -1;
                         }
@@ -192,7 +180,7 @@ namespace tcode { namespace io {
         wake_up();
     }
 
-    void select::execute( tcode::operation* op ) {
+    void rts::execute( tcode::operation* op ) {
         do {
             tcode::threading::spinlock::guard guard(_lock);
             op_add(op);
@@ -200,8 +188,8 @@ namespace tcode { namespace io {
         wake_up();
     }
 
-    void select::connect( 
-            select::descriptor& desc 
+    void rts::connect( 
+            rts::descriptor& desc 
             , ip::tcp::operation_connect_base* op )
     {
         int fd = socket( op->address().family() , SOCK_STREAM , IPPROTO_TCP );
@@ -215,7 +203,7 @@ namespace tcode { namespace io {
                 r = ::connect( fd , op->address().sockaddr() , op->address().sockaddr_length());
             }while((r == -1) && (errno == EINTR));
             if ( (r == 0) || (errno == EINPROGRESS )){
-                desc = new select::_descriptor(this , fd );
+                desc = new rts::_descriptor(this , fd );
                 desc->op_queue[tcode::io::ev_connect].push_back( op );
                 if ( bind( desc ) ){
                     return;
@@ -231,31 +219,31 @@ namespace tcode { namespace io {
         execute(op);
     }
 
-    void select::write( select::descriptor desc 
+    void rts::write( rts::descriptor desc 
             , ip::tcp::operation_write_base* op)
     {
         desc->add_ref();
         execute( tcode::operation::wrap(
             [this,desc,op]{
                 desc->op_queue[tcode::io::ev_write].push_back(op);
-                desc->complete( this , ev_write );
+                desc->complete( this , EPOLLOUT );
                 desc->release(this);
             }));
     }
 
-    void select::read( descriptor desc
+    void rts::read( descriptor desc
             , ip::tcp::operation_read_base* op ){
         desc->add_ref();
         execute( tcode::operation::wrap(
             [this,desc,op]{
                 desc->op_queue[tcode::io::ev_read].push_back(op);
-                desc->complete( this , ev_read );
+                desc->complete( this , EPOLLIN );
                 desc->release(this);
             }));
 
     }
     
-    bool select::listen( descriptor& desc 
+    bool rts::listen( descriptor& desc 
                 , const ip::address& addr )
     {
         int fd = socket( addr.family() , SOCK_STREAM , IPPROTO_TCP );
@@ -266,7 +254,7 @@ namespace tcode { namespace io {
             reuse.set_option( fd );
             if ((::bind( fd , addr.sockaddr() , addr.sockaddr_length() ) == 0)
                     && (::listen(fd, SOMAXCONN ) == 0 )) {
-                desc = new select::_descriptor(this,fd);
+                desc = new rts::_descriptor(this,fd);
                 if ( bind( desc ))
                     return true;
                 delete desc;
@@ -277,30 +265,31 @@ namespace tcode { namespace io {
         return false;
     }
 
-    void select::accept( descriptor desc
+    void rts::accept( descriptor desc
             , int
             , ip::tcp::operation_accept_base* op ){
         if ( desc == nullptr ){
             //todo
             return;
         }
+
         desc->add_ref();
         execute( tcode::operation::wrap(
             [this,desc,op]{
                 desc->op_queue[tcode::io::ev_accept].push_back(op);
-                desc->complete( this , ev_read );
+                desc->complete( this , EPOLLIN );
                 desc->release(this);
             }));
     }
-
-    bool select::open( descriptor& desc
+    
+    bool rts::open( descriptor& desc
                 , int af , int type , int proto )
     {
         int fd = socket( af , type , proto );
         if ( fd != -1 ) {
             tcode::io::ip::option::non_blocking nb;
             nb.set_option(fd);
-            desc = new select::_descriptor( this , fd );
+            desc = new rts::_descriptor( this , fd );
             if ( bind(desc))
                 return true;
             delete desc;
@@ -309,7 +298,9 @@ namespace tcode { namespace io {
         }
         return false;
     }
-    bool select::bind( descriptor& desc
+        
+    
+    bool rts::bind( descriptor& desc
             , const ip::address& addr ) 
     {
         int fd = socket( addr.family() , SOCK_DGRAM , IPPROTO_UDP );
@@ -319,7 +310,7 @@ namespace tcode { namespace io {
             tcode::io::ip::option::reuse_address reuse( true );
             reuse.set_option( fd );
             if ( ::bind( fd , addr.sockaddr() , addr.sockaddr_length() ) == 0 ){
-                desc = new select::_descriptor( this , fd );
+                desc = new rts::_descriptor( this , fd );
                 if ( bind(desc))
                     return true;
                 delete desc;
@@ -330,13 +321,13 @@ namespace tcode { namespace io {
         return false;
     }
 
-    void select::write( descriptor& desc 
+    void rts::write( descriptor& desc 
             , ip::udp::operation_write_base* op )
     {   
         if ( desc == nullptr ) {
             int fd = socket( op->address().family() , SOCK_DGRAM , IPPROTO_UDP);
             if ( fd != -1 ){
-                desc = new select::_descriptor( this , fd );
+                desc = new rts::_descriptor( this , fd );
                 if ( !bind( desc )){
                     op->error() = tcode::last_error();
                     delete desc;
@@ -355,12 +346,12 @@ namespace tcode { namespace io {
         execute( tcode::operation::wrap(
             [this,desc,op]{
                 desc->op_queue[tcode::io::ev_write].push_back(op);
-                desc->complete( this , ev_write );
+                desc->complete( this , EPOLLOUT );
                 desc->release(this);
             }));
     }
     
-    void select::read( descriptor desc
+    void rts::read( descriptor desc
             , ip::udp::operation_read_base* op )
     {
         if ( desc == nullptr ) {
@@ -371,23 +362,23 @@ namespace tcode { namespace io {
         execute( tcode::operation::wrap(
             [this,desc,op]{
                 desc->op_queue[tcode::io::ev_read].push_back(op);
-                desc->complete( this , ev_read);
+                desc->complete( this , EPOLLIN);
                 desc->release(this);
             }));
     }
 
 
-    void select::op_add( tcode::operation* op ){
+    void rts::op_add( tcode::operation* op ){
         _op_queue.push_back( op );
         _engine.active_inc();
     }
 
-    void select::op_run( tcode::operation* op ){
+    void rts::op_run( tcode::operation* op ){
         _engine.active_dec();
         (*op)();
     }
 
-    int select::writev( descriptor desc 
+    int rts::writev( descriptor desc 
             , tcode::io::buffer* buf , int cnt
             , std::error_code& ec )
     {
@@ -407,7 +398,7 @@ namespace tcode { namespace io {
         return -1;
     }
 
-    int select::readv( descriptor desc 
+    int rts::readv( descriptor desc 
             , tcode::io::buffer* buf , int cnt
             , std::error_code& ec )
     {
@@ -431,7 +422,7 @@ namespace tcode { namespace io {
         return -1;
     }
 
-    int select::accept( descriptor listen 
+    int rts::accept( descriptor listen 
             , descriptor& accepted 
             , ip::address& addr
             , std::error_code& ec )
@@ -449,7 +440,7 @@ namespace tcode { namespace io {
                 tcode::io::ip::option::non_blocking nb;
                 nb.set_option( fd );
 
-                accepted = new select::_descriptor( this , fd );
+                accepted = new rts::_descriptor( this , fd );
                 bind( accepted );
                 return 0;
             }
@@ -461,7 +452,7 @@ namespace tcode { namespace io {
         return -1;
     }
 
-    int select::read( descriptor desc 
+    int rts::read( descriptor desc 
             , tcode::io::buffer& buf 
             , tcode::io::ip::address& addr 
             , std::error_code& ec )
@@ -483,7 +474,7 @@ namespace tcode { namespace io {
         return -1;
     }
 
-    int select::write( descriptor desc 
+    int rts::write( descriptor desc 
             , const tcode::io::buffer& buf 
             , const tcode::io::ip::address& addr 
             , std::error_code& ec )
@@ -504,9 +495,8 @@ namespace tcode { namespace io {
         }
         return -1;
     }
-
-    int select::native_descriptor( descriptor d ) {
-        return d->fd;
+    
+    int rts::native_descriptor( descriptor desc ) {
+        return desc->fd;
     }
 }}
-
